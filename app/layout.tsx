664e5a10fd6f6a861abf65784da9759deb40212a
Jayasriraam-s <jayasriraam27@gmail.com>
2024-09-13 21:23:38 +0530

    Change all things

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 import type { Metadata } from "next";
 import { Syne } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const syne = Syne({
   subsets: ["latin"],
@@ -90,6 +91,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
