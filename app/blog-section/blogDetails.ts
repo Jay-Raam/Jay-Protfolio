deac20278be1481d13a9a979f1d9350b7d99a8a1
Jayasriraam-s <jayasriraam27@gmail.com>
2024-10-11 20:48:24 +0530

    Add blog commit

--- app/blog-section/blogDetails.ts
+++ app/blog-section/blogDetails.ts
@@ -32,5 +32,31 @@ export const blogDetails = [
     url: "https://jayasriraam.blogspot.com/2024/02/blog-post.html",
     available: true,
   },
-
+  {
+    title:
+      "முதல் சந்திப்பு",
+    image:
+      require(".//../../public/blog4.png"),
+    date: "Oct 2024",
+    url: "https://jayasriraam.blogspot.com/2024/09/blog-post.html",
+    available: true,
+  },
+  {
+    title:
+      "காதல் - ஒரு இன்ப சுற்றுலா",
+    image:
+      require(".//../../public/blog5.png"),
+    date: "Oct 2024",
+    url: "https://jayasriraam.blogspot.com/2024/10/blog-post.html",
+    available: true,
+  },
+  {
+    title:
+      "AOS - Animation Library",
+    image:
+      require(".//../../public/image6.webp"),
+    date: "Oct 2024",
+    url: "https://jayasriraam.blogspot.com/2024/10/aos-animate-on-scroll-library.html",
+    available: true,
+  },
 ];
