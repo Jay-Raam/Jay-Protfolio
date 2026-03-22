7ddfe349eddf8843028cc9647f683b21289d2bb9
Jay-Raam <jayasriram.job@gmail.com>
2026-03-22 09:18:00 +0530

    New Project Added

--- app/hero-section/Hero.tsx
+++ app/hero-section/Hero.tsx
@@ -4,7 +4,7 @@ import { monaSans } from "../fonts/monaSans";
 import { motion } from "framer-motion";
 import { imageAnimation, bodyAnimation } from "../animations/animations";
 import AnimatedWords from "../animations/AnimatedWords";
-import profile from "../../public/m2.png";
+import profile from "../../public/jayasriraam.jpg";
 
 const Hero = () => {
   return (
